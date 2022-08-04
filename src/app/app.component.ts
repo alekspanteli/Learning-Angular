f3ea6364376538dcd6873b697a741fc2562c328a
alekspanteli <alekspanteli@rambler.ru>
2022-08-04 15:47:35 +0300

    add inline template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  template: `
+  <h1>Artist directory</h1>
+  <p>Hello</p>
+  `,
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
